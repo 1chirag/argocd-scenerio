ed0f9d4f2110b130d3db8c043f08f048e0e0663b
1chirag <e19cse195@bennett.edu.in>
2025-08-03 12:27:28 +0530

    NodeAPI, Kubernetes, Docker Files

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World my name is chirag jain. today is friendship day and I am happy to be your friend!');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
