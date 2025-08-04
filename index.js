f56a95010f4591ef03e7e96c546521386e2461ea
1chirag <e19cse195@bennett.edu.in>
2025-08-04 18:42:45 +0530

    checking

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hi, Im a devops engineer. Lets check argocd with helm , now both are in diffrent branch lets see check if it works!');
+  res.send('Hi, Im a devops engineer. Lets check argocd with helm , now both are in diffrent branch lets see check if it works! still 3 times');
 });
 
 app.listen(PORT, '0.0.0.0', () => {
