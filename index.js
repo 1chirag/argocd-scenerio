8a206678e38ad8b40a92fb4afd9f95e67bec8b53
1chirag <e19cse195@bennett.edu.in>
2025-08-05 21:15:40 +0530

    day 3 testing

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Today, the complete CI/CD workflow was successfully set up and verified. Developers are pushing their application code to Repository 1, which triggers a Jenkins pipeline. Jenkins builds a Docker image from the latest code and pushes it to Docker Hub with an appropriate tag. It then updates the values.yaml file in the Helm chart repository with the new image tag and commits the changes. This Helm repository is being watched by ArgoCD, which automatically detects the updates and syncs the deployment to the Kubernetes cluster. As a result, a fully automated and reliable CI/CD pipeline is now in place, enabling seamless code deployment from development to production.');
-});
+  res.send('day 3 , offc se agaya hoon ')});
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
