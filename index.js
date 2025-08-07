b9ba198b8f124665eb53dc8a91d28822b52a9c02
1chirag <e19cse195@bennett.edu.in>
2025-08-07 17:21:35 +0530

    day 4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('day 3 , offc se agaya hoon ')});
+  res.send('day 4 ashoknagar agaya hoon mai  ')});
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
